e69722c2b2791978cc6ed3391b8b16f13d156c06
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-01-06 08:41:23 -0500

    Prevent exclusion of any class files for jar library in resulting apk/odex

--- Android.mk
+++ Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_JAR_EXCLUDE_FILES := none
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := cordova:libs/cordova-2.1.0.jar
 include $(BUILD_MULTI_PREBUILT)
 
@@ -9,8 +10,8 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under)
 LOCAL_PACKAGE_NAME := org.wikipedia
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := cordova:libs/cordova-2.1.0.jar
 LOCAL_STATIC_JAVA_LIBRARIES := cordova
+LOCAL_JAR_EXCLUDE_FILES := none
 LOCAL_MODULE_TAGS := optional
 LOCAL_CERTIFICATE := platform
 
